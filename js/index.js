2c6694966549c7dde7e4320fedc8ce9b4b1216a1
Enrique Mejías Álvarez <enrique.mejias@envato.com>
2021-03-25 21:34:00 -0600

    Finish the work

---
+++ js/index.js
@@ -0,0 +1,26 @@
+const navCont = document.querySelector('.navigation-container');
+
+const menuIcon = document.querySelector('.menu');
+const closeIcon = document.querySelector('.close');
+
+let activeLink = document.querySelector('li.active');
+
+const links = document.querySelector('.links');
+
+links.addEventListener('click', (e) => {
+    const element = e.target;
+
+    if (element.classList.contains('link') === false) return;
+
+    activeLink.classList.remove('active');
+    element.classList.add('active');
+    activeLink = element;
+})
+
+menuIcon.addEventListener('click', () => {
+    navCont.classList.add('active');
+});
+
+closeIcon.addEventListener('click', () => {
+    navCont.classList.remove('active');
+});
